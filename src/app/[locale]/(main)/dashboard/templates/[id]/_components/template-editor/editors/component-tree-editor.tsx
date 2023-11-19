ba3ba263f6442f7ee201caf5fcf7e3328e7ffec1
drepkovsky <dominik.repkovsky@gmail.com>
2023-11-19 20:05:11 +0100

    fix(component-tree-editor): fix component tree order

--- src/app/[locale]/(main)/dashboard/templates/[id]/_components/template-editor/editors/component-tree-editor.tsx
+++ src/app/[locale]/(main)/dashboard/templates/[id]/_components/template-editor/editors/component-tree-editor.tsx
@@ -14,6 +14,7 @@ import {
   useSelectedComponent,
 } from "~/app/[locale]/(main)/dashboard/templates/[id]/_atoms/template-editor-atoms";
 import { Badge } from "~/app/_components/ui/badge";
+import { cn } from "~/app/_utils/styles-utils";
 import { getTemplateComponentParentById } from "~/shared/invoice-template/invoice-template-helpers";
 import { type InvoiceTemplateChild } from "~/shared/invoice-template/invoice-template-schemas";
 
@@ -44,11 +45,8 @@ export function ComponentTreeEditor() {
   );
 
   const siblings = useMemo(
-    () =>
-      (parent && "children" in parent ? parent.children ?? [] : []).filter(
-        (sibling) => sibling.id !== selectedComponent.id,
-      ),
-    [parent, selectedComponent.id],
+    () => (parent && "children" in parent ? parent.children ?? [] : []),
+    [parent],
   );
 
   return (
@@ -65,47 +63,41 @@ export function ComponentTreeEditor() {
           {parent.id}
         </Badge>
       )}
-      <Badge
-        onClick={() => setSelectedComponent(selectedComponent.id)}
-        variant="outline"
-        aria-disabled={true}
-        className="pointer-events-none ml-4 gap-2 border-primary opacity-70"
-        onMouseOver={() => setHighlightedComponent(selectedComponent.id)}
-        onMouseOut={() => setHighlightedComponent(null)}
-      >
-        {TYPE_TO_ICON[selectedComponent.type]}
-        {selectedComponent.id}
-      </Badge>
-
-      {children?.map((child) => {
-        return (
-          <Badge
-            key={child.id}
-            onClick={() => setSelectedComponent(child.id)}
-            variant="secondary"
-            className="ml-8 cursor-pointer gap-2"
-            onMouseOver={() => setHighlightedComponent(child.id)}
-            onMouseOut={() => setHighlightedComponent(null)}
-          >
-            {TYPE_TO_ICON[child.type]}
-            {child.id}
-          </Badge>
-        );
-      })}
 
       {siblings?.map((sibling) => {
         return (
-          <Badge
-            key={sibling.id}
-            onClick={() => setSelectedComponent(sibling.id)}
-            variant="secondary"
-            className="ml-4 cursor-pointer gap-2"
-            onMouseOver={() => setHighlightedComponent(sibling.id)}
-            onMouseOut={() => setHighlightedComponent(null)}
-          >
-            {TYPE_TO_ICON[sibling.type]}
-            {sibling.id}
-          </Badge>
+          <>
+            <Badge
+              key={sibling.id}
+              onClick={() => setSelectedComponent(sibling.id)}
+              variant="secondary"
+              className={cn("ml-4 cursor-pointer gap-2", {
+                "pointer-events-none ml-4 gap-2 border-primary opacity-70":
+                  sibling.id === selectedComponent.id,
+              })}
+              onMouseOver={() => setHighlightedComponent(sibling.id)}
+              onMouseOut={() => setHighlightedComponent(null)}
+            >
+              {TYPE_TO_ICON[sibling.type]}
+              {sibling.id}
+            </Badge>
+            {selectedComponent.id === sibling.id &&
+              children?.map((child) => {
+                return (
+                  <Badge
+                    key={child.id}
+                    onClick={() => setSelectedComponent(child.id)}
+                    variant="secondary"
+                    className="ml-8 cursor-pointer gap-2"
+                    onMouseOver={() => setHighlightedComponent(child.id)}
+                    onMouseOut={() => setHighlightedComponent(null)}
+                  >
+                    {TYPE_TO_ICON[child.type]}
+                    {child.id}
+                  </Badge>
+                );
+              })}
+          </>
         );
       })}
     </div>
