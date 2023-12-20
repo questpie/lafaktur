d9bcc4d79a96137c4d8cf8f9b20eb3069fd37f1a
drepkovsky <dominik.repkovsky@gmail.com>
2023-12-20 13:10:47 +0100

    chore: bump deps

--- src/app/[locale]/(main)/dashboard/invoices/page.tsx
+++ src/app/[locale]/(main)/dashboard/invoices/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 
 import { useTranslations } from "next-intl";
+import { useRouter } from "next-nprogress-bar";
 import { LuPlus } from "react-icons/lu";
 import { useHeader } from "~/app/[locale]/(main)/dashboard/_components/header";
 import InvoiceDataTable from "~/app/[locale]/(main)/dashboard/invoices/_components/invoice-data-table";
+import { api } from "~/trpc/react";
 
 export default function InvoicePage() {
   const t = useTranslations();
+  const router = useRouter();
+
+  const addMutation = api.invoice.create.useMutation();
 
   useHeader({
     title: t("invoice.invoicePage.title"),
     mainAction: {
       label: t("invoice.invoicePage.mainAction"),
       href: "/dashboard/invoices/add",
+      onClick: () => {
+        // addMutation.mutate({});
+      },
       icon: <LuPlus />,
     },
   });
